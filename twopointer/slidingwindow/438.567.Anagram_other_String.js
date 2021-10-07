57a07103f50df247430037afa4dfda136e9bd01a
yuyu <yheimbusi@gmail.com>
2021-10-07 16:35:37 -0700

    review sliding window

--- twopointer/slidingwindow/438.567.Anagram_other_String.js
+++ twopointer/slidingwindow/438.567.Anagram_other_String.js
@@ -1,7 +1,5 @@
 /*
 438.Find All Anagrams in a String
-
-
 Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 Example 1:
@@ -10,58 +8,61 @@ Output: [0,6]
 Explanation:
 The substring with start index = 0 is "cba", which is an anagram of "abc".
 The substring with start index = 6 is "bac", which is an anagram of "abc".
-
-
-Window size is fixed: p.
-Left pointer will move foward when window moves
-Two pointer starting from left towards right
-normally, Sliding window: T:O(n) S:O(n)
 */
 
-const asscii = "a".charCodeAt();
+/*
+LOGIC: 
+1.window size is fixed
+2.create hash for p {c:freq}
+3.'count' variable for counting valid result, since there are more than one result, so will reset 'count' while changing window(move left action)
+4.when move left? i > p.length -1, out of p
 
+Complexity: T:O(n) S:O(p)
+
+*/
+const a = "a".charCodeAt();
 var findAnagrams = function (s, p) {
-  //corner case or check input valid
-  if (s.length < p.length) return [];
+  if (
+    p == null ||
+    s == null ||
+    p.length == 0 ||
+    s.length == 0 ||
+    p.length > s.length
+  )
+    return [];
 
-  //use data structure eg.array, map store specific elements in window {char: freq}
-  const hash = new Array(26).fill(0);
-  for (let i = 0; i < p.length; i++) {
-    hash[p.charCodeAt(i) - asscii]++;
+  //create hash
+  //only lower case
+  const dic = new Array(26).fill(0);
+  for (let c of p) {
+    dic[c.charCodeAt() - a]++;
   }
 
-  //declare result, left pointer(start of window), count(current situation)
-  const res = [];
-  let left = 0,
-    count = 0;
+  //create variable
+  let res = [],
+    count = 0,
+    left = 0;
+  for (let i = 0; i < s.length; i++) {
+    //update hash
+    dic[s.charCodeAt(i) - a]--;
 
-  //loop for window right for expanding
-  for (let right = 0; right < s.length; right++) {
-    //now used it, so update(deduct amount in hash) if after deduct is -1, mean not in pstr
-    hash[s.charCodeAt(right) - asscii]--;
+    //check valid or not
+    if (dic[s.charCodeAt(i) - a] >= 0) count++;
 
-    //check valid char in pstr
-    if (hash[s.charCodeAt(right) - asscii] >= 0) {
-      count++;
-    }
-
-    //check if need to move left, need keep window size is p length
-    if (right > p.length - 1) {
-      //add it again since deduct
-      hash[s.charCodeAt(left) - asscii]++;
-      //deduct count since was valid
-      if (hash[s.charCodeAt(left) - asscii] > 0) {
-        count--;
-      }
-
+    //move left
+    if (i > p.length - 1) {
+      dic[s.charCodeAt(left) - a]++; //recover
+      if (dic[s.charCodeAt(left) - a] > 0) count--; //count reset since need use it again, > 0 since = 0 means no cha in p
       left++;
     }
 
-    if (count == p.length) res.push(left);
+    //check result
+    if (count == p.length) {
+      res.push(left);
+    }
   }
   return res;
 };
-
 // console.log(findAnagrams("acba", "ab"));
 
 /*
