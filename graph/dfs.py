32803757b7778baa19fa3ebd4ee49cae0ee69e7b
yuyu <yheimbusi@gmail.com>
2021-10-29 12:21:26 -0700

    graph

---
+++ graph/dfs.py
@@ -0,0 +1,24 @@
+#200. Number of Islands
+def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0] or len(grid) == 0 or len(grid[0]) == 0: return 0
+        
+        island = 0
+        
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+                    return
+            grid[i][j] = "#"
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    # starting new island
+                    island += 1
+                    dfs(i, j)
+        
+        return island
