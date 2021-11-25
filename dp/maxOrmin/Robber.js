5aadec644240ca4b4da0447f9222e730add9a80f
yuyu <yheimbusi@gmail.com>
2021-11-25 10:20:19 -0800

    dp

--- dp/maxOrmin/Robber.js
+++ dp/maxOrmin/Robber.js
@@ -56,4 +56,38 @@ var deleteAndEarn = function (nums) {
   }
   return profit;
 };
-console.log(deleteAndEarn([1]));
+// console.log(deleteAndEarn([1]));
+
+//213. House Robber II
+var rob2 = function (nums) {
+  //two pointe:[1,2,3,1] 1:1+3(4) 2:2+1(3)
+  //first=[0] to [len-1] dp1=> profit second=[1] to [len] dp2=> profit, compare two profits
+  if (nums.length == 1) return nums[0];
+  if (nums == null || nums.length == 0) return 0;
+  if (nums.length == 2) return Math.max(nums[0], nums[1]);
+
+  const first = new Array(nums.length).fill(0);
+  let p1 = 0;
+  first[0] = nums[0];
+  first[1] = Math.max(first[0], nums[1]);
+
+  for (let i = 2; i < nums.length - 1; i++) {
+    first[i] = Math.max(first[i - 1], first[i - 2] + nums[i]);
+    p1 = Math.max(p1, first[i]);
+  }
+  const second = new Array(nums.length).fill(0);
+  let p2 = 0;
+  second[1] = nums[1];
+  second[2] = Math.max(second[1], nums[2]);
+
+  if (nums.length == 3) return Math.max(first[1], second[2]);
+  for (let i = 3; i < nums.length; i++) {
+    second[i] = Math.max(second[i - 1], second[i - 2] + nums[i]);
+    p2 = Math.max(p2, second[i]);
+  }
+
+  return Math.max(p1, p2);
+};
+console.log(rob2([1, 2, 3, 1]));
+console.log(rob2([1, 2, 3]));
+console.log(rob2([1, 1]));
--- greedy/45.55.Jump_Game.py
+++ greedy/45.55.Jump_Game.py
@@ -14,7 +14,7 @@ def canJump(nums):
             if maxIndex >= len(nums) - 1: return True
             
         return False
-print(canJump([3,2,1,0,4])) #[0]=>true [3,2,1,0,4]=>false [0,2,3]=>false
+# print(canJump([3,2,1,0,4])) #[0]=>true [3,2,1,0,4]=>false [0,2,3]=>false
 
 '''
 45.Jump Game II
@@ -22,18 +22,28 @@ print(canJump([3,2,1,0,4])) #[0]=>true [3,2,1,0,4]=>false [0,2,3]=>false
     1.greedy
     2.set jumpNo, endIndex, farIndex
 '''
-def jump(self, nums: List[int]) -> int:
-        if not nums or len(nums) == 0: return 0
+def jump2(nums):
+    '''
+    LOGIC: greedy
+    HOW:
+    1.step var will calculate minimum jump
+    2.endIndex track when finish cur jump
+    3.always update endIndex into farIndex, so jump less
+    '''
+    if not nums or len(nums) == 0: return 0
         
-        jump = 0
-        endIndex = 0
-        farIndex = 0
+    jump = 0
+    farIndex = 0
+    endIndex = 0 #when start next jump
+    for i in range(len(nums)-1): # no need consider lastone since no jump at that point
+        farIndex = max(farIndex, nums[i] + i)
+        if i == endIndex: # finish one jump,start new jump
+            jump += 1
+            endIndex = farIndex
+                
+            if endIndex >= len(nums)-1: return jump
+    return jump
+    
+print(jump2([2,3,1,1,4]))
+
         
-        for i in range(0, len(nums)-1, 1): #no need go through last one, since alway can reach lastindex
-            farIndex = max(farIndex, i+nums[i])
-            if i == endIndex:
-                # need another jump
-                jump += 1
-                endIndex = farIndex
-        return jump
-            
