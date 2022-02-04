ef6e84b303cc2423048351499765224ef341c0e0
yuyu <yheimbusi@gmail.com>
2022-02-03 23:52:11 -0800

    sum

--- Math/1995. Count Special Quadruplets.py
+++ Math/1995. Count Special Quadruplets.py
@@ -19,3 +19,19 @@
                                 if nums[y] + nums[k] + nums[j] == nums[i]:
                                     res += 1
         return res
+    
+    # O(n^2)
+    def countQuadruplets(self, nums: List[int]) -> int:
+        count = collections.defaultdict(int)
+        l = len(nums)
+        count[nums[l-1] - nums[l-2]] = 1
+        res = 0
+        
+        for b in range(l-3, 0, -1):
+            for a in range(b-1, -1, -1):
+                res += count[nums[a] + nums[b]]
+            
+            for x in range(l-1, b, -1):
+                count[nums[x] - nums[b]] += 1
+        return res
+    
